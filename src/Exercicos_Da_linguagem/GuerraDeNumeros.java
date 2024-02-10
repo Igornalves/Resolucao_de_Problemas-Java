c3d77420ab64f8cd1333736d98142c24a2ea84c1
igor_alves <igornalves08@gmail.com>
2024-02-10 16:25:29 -0300

    fazendo novas alteeracoes em exercicios

---
+++ src/Exercicos_Da_linguagem/GuerraDeNumeros.java
@@ -0,0 +1,24 @@
+package Exercicos_Da_linguagem;
+
+public class GuerraDeNumeros {
+
+    // Crie uma função que receba um array de inteiros, some os números pares e ímpares separadamente e depois retorne a diferença entre a soma dos números pares e ímpares.
+
+    // Exemplos
+    //     warOfNumbers([2, 8, 7, 5]) ➞ 2
+    //     2 + 8 = 10
+    //     7 + 5 = 12
+    //     12 is larger than 10
+    //     So we return 12 - 10 = 2
+
+    //     warOfNumbers([12, 90, 75]) ➞ 27
+
+    //     warOfNumbers([5, 9, 45, 6, 2, 7, 34, 8, 6, 90, 5, 243]) ➞ 168
+    
+    // Notas
+    // A matriz fornecida contém apenas números inteiros positivos.
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Exercicos_Da_linguagem/RecursãoSomaDeArray.java
@@ -0,0 +1,26 @@
+package Exercicos_Da_linguagem;
+
+/**
+ * RecursãoSomaDeArray
+ */
+public class RecursãoSomaDeArray {
+
+    // Escreva uma função que encontre a soma de um array. Torne sua função recursiva.
+
+    // Exemplos
+    // sum([1, 2, 3, 4]) ➞ 10
+
+    // sum([1, 2]) ➞ 3
+
+    // sum([1]) ➞ 1
+
+    // sum([]) ➞ 0
+
+    // Notas:
+    // Retorne 0para um array vazio.
+    // Verifique a guia Recursos para obter informações sobre recursão.
+
+    public static void main(String[] args) {
+        
+    }
+}
